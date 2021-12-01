623a9d208a7d416e984d380fc0baa4f3a8c6aa16
Stevie Ambroise <s.ambroise@protonmail.com>
2021-11-30 18:51:09 -0700

    Complete deck test file

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -12,13 +12,29 @@ describe('Deck', function() {
         expect(Deck).to.be.a('function');
     });
 
-    it('should be an instance of Deck', function() {
+    it('should be  an instance of Deck', function() {
         const deck = new Deck();
 
         expect(deck).to.be.an.instanceOf(Deck);
     });
 
-    it('should have a collection of playable cards')
+    it('should store a collection of playable card objects', function() {
+        const card1 = new Card();
+        const card2 = new Card();
+        const card3 = new Card();
 
+        const deck = new Deck([card1, card2, card3]);
 
+        expect(deck.cards).to.deep.equal([card1, card2, card3]);
+    });
+
+    it('should be able to return number of cards in the deck', function() {
+        const card1 = new Card();
+        const card2 = new Card();
+        const card3 = new Card();
+        
+        const deck = new Deck([card1, card2, card3]);
+
+        expect(deck.countCards()).to.equal(3);
+    })
 });
